1990a0eea47dae3eae12ff9a3bec03c75f2e845c
KhangDP <pdkhangpc1304@gmail.com>
2026-05-25 19:20:44 +0700

    First up

---
+++ src/main/java/com/dkp/dkp_workspace/DkpWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.dkp.dkp_workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkpWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkpWorkspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dkp/dkp_workspace/DkpWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkp.dkp_workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkpWorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
